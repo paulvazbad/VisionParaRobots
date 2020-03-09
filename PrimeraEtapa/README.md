874c7d7193b72be077c04c187657f91f7eea1413
Paul Vazquez <pev@live.com.mx>
2020-03-09 17:26:33 -0600

    Histogram 3 arrays function

---
+++ PrimeraEtapa/README.md
@@ -0,0 +1,9 @@
+# Installation
+Read manual
+
+# Compiling
+To compile the program simply do:
+
+    make
+
+This will compile the program and move the executable to the 'bin' folder.
---
+++ PrimeraEtapa/src/main.cpp
@@ -0,0 +1,96 @@
+/*
+ * Title: Flipped Image Sample (Cascaron)
+ * Class: Vision para Robot
+ * Instructor: Dr. Jose Luis Gordillo (http://LabRob.mty.itesm.mx/)
+ * Code: Manlio Barajas (manlito@gmail.com), Alberto Jahuey (A01039835@itesm.mx)
+ * Institution: Tec de Monterrey, Campus Monterrey
+ * Date: January 10, 2012 Last Update: January 30, 2019
+ *
+ * Description: This program takes input from a camera (recognizable by
+ * OpenCV) and it flips it horizontally.
+ * "Basic" version uses frequently the "Cv::Mat::At" method which slows down
+ * performance. This program has illustrative purposes, provided the existence
+ * of cv::flip method.
+ *
+ *
+ * This programs uses OpenCV http://opencv.willowgarage.com/wiki/
+ */
+
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <koolplot.h>
+using namespace std;
+using namespace cv;
+
+void showCamera(){
+  VideoCapture camera = VideoCapture(0);
+  bool isCameraAvailable = camera.isOpened();
+
+  /* Create images where captured and transformed frames are going to be stored
+   */
+  Mat currentImage;
+  Mat flippedImage;
+  /* Clean the terminal */
+  cout << "\033[2J\033[1;1H";
+  cout << "Basic Show Image \t|\tUse 'x' or 'Esc' to terminate execution\n";
+  while (true) {
+    /* Obtain a new frame from camera */
+    if (isCameraAvailable) {
+      camera.read(currentImage);
+    } else {
+      currentImage = imread("PlaceholderImage.jpg", CV_LOAD_IMAGE_COLOR);
+    }
+
+    if (currentImage.size().width <= 0 && currentImage.size().height <= 0) {
+      cout << "ERROR: Camera returned blank image, check connection\n";
+      break;
+    }
+    /* If 'x' is pressed, exit program */
+    char key = waitKey(1);
+    if(key == 'x' || key == 27 ){ // 27 = ESC
+      break;
+    }
+    imshow("Imagen",currentImage);
+}
+}
+void initializeMat(double RGBValues[3][256]){
+  for(int color_value = 0; color_value<256; color_value++){
+    for(int color=0; color<3; color++){
+      RGBValues[color][color_value]=0;
+    }
+  }
+  
+}
+
+void plotHist(double RGBValues[3][256]){
+    
+}
+
+void RGBHist(const Mat &Image,double RGBValues[3][256]){
+  initializeMat(RGBValues);
+  for(int row=0; row < Image.rows; ++row){
+    for(int col=0; col < Image.cols; ++col){
+      for (int i = 0; i < Image.channels(); ++i)
+			{
+        int color_value = Image.at<Vec3b>(row, col)[i];
+				RGBValues[i][color_value]++;
+			}
+    }
+  }
+}
+
+void printValue(double ColorValues[256]){
+  for(int i=0; i<256; i++){
+    cout<<ColorValues[i]<<endl;
+  }
+}
+int main(int argc, char *argv[]) {
+  /* First, open camera device */
+    Mat currentImage = imread("PlaceholderImage.jpg", CV_LOAD_IMAGE_COLOR);
+    double RGBValues[3][256];
+    RGBHist(currentImage,RGBValues);
+    printValue(RGBValues[2]);
+    
+  }
